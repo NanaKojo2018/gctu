110a8219a06d53f71eec3469c0d6a13c7d198b7d
NanaKojo2018 <55985283+NanaKojo2018@users.noreply.github.com>
2026-09-13 13:17:30 +0100

    Delete kyc-check-nextjs-main directory

--- kyc-check-nextjs-main/packages/api/src/utils/imageUtils.ts
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-
-export const cleanupFiles = (filePaths: string[]): void => {
-  filePaths.forEach((filePath) => {
-    if (fs.existsSync(filePath)) {
-      fs.unlinkSync(filePath);
-    }
-  });
-};
